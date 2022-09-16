7897f28e939543582b1eafb1f9742fd9edc87456
jessymatta <jessy.w.matta@gmail.com>
2022-09-16 22:42:39 +0300

    added eventListener to each individual tile

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("working?");
+
+// getting all grid tiles and storing them in a "tiles"
+const tiles = document.querySelectorAll(".tile");
+console.log(tiles);
+
+// Event listener for each
+// Add event listener
+tiles.forEach((tile, index) => {
+    tile.addEventListener("click", () => {
+      console.log(tile,tiles);
+    });
+  });
